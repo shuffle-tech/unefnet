651313f7ea97e538c9fa8baeed1648e30b5268d2
Shuffle Tech <shuffle-tech@googlegroups.com>
2011-07-12 09:30:29 -0300

    unefnet

--- shuffle-core/security/src/main/java/com/shuffle/security/core/IShuffleUserDao.java
+++
@@ -1,5 +0,0 @@
-package com.shuffle.security.core;
-
-public interface IShuffleUserDao {
-
-}
--- shuffle-core/security/src/main/java/com/shuffle/security/core/ShuffleUserDao.java
+++
@@ -1,6 +0,0 @@
-package com.shuffle.security.core;
-
-
-public class ShuffleUserDao implements IShuffleUserDao {
-
-}
